1c11ff8162278b0f19d6cb02c7ae76c723ea3dc5
Gabi <mgluisa9@gmail.com>
2024-05-24 18:04:30 +0300

    curs24 final

---
+++ src/main/java/pageObjects/MenuPage.java
@@ -0,0 +1,38 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class MenuPage {
+
+	public WebDriver driver;
+	public MenuPage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	//locatori/webelements
+	/*
+	 * driver.findElement(By.linkText("Login"))
+	 * */
+	public By loginLink = By.linkText("Login");
+	public By contactsLink = By.linkText("CONTACTS");
+	
+	//WebElement loginButton = driver.findElement(By.linkText("Login"));
+	//WebElement contactsButton = driver.findElement(By.linkText("CONTACTS"));
+	
+	//metode specifice functionalitatii pe care o au
+	public void navigateTo(By locator) {
+		driver.findElement(locator).click();
+	}
+	
+	//navigateTo(contactsLink)
+	//navigateTo(loginLink)
+	
+	/*public void navigateToLogin() {
+		loginButton.click();
+	}
+	public void navigateToContacts() {
+		loginButton.click();
+	}*/
+}
---
+++ src/main/java/tests/LoginTest.java
@@ -0,0 +1,42 @@
+package tests;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+import Framework.utils.PropertiesFileProcessor;
+import Selenium.utils.BaseTest;
+import pageObjects.LoginPage;
+import pageObjects.MenuPage;
+
+public class LoginTest extends BaseTest{
+
+	String USER = PropertiesFileProcessor.readPropertiesFile("user", "credentials.Properties");
+	String PASS = PropertiesFileProcessor.readPropertiesFile("pass", "credentials.Properties");
+	
+	@Test(priority=1)
+	public void loginTest() throws InterruptedException {
+		MenuPage menu = new MenuPage(driver);
+		LoginPage login = new LoginPage(driver);
+		
+		menu.navigateTo(menu.loginLink);
+		login.loginInApp(USER,PASS);
+		Thread.sleep(500);
+		assertTrue(login.checkMsgIsDisplayed(login.successLoginMsg));
+		Thread.sleep(3000);
+		login.logoutFromApp();
+		
+	}
+	
+	@Test(priority=2)
+	public void invalidloginTest() throws InterruptedException {
+		MenuPage menu = new MenuPage(driver);
+		LoginPage login = new LoginPage(driver);
+		
+		menu.navigateTo(menu.loginLink);
+		login.loginInApp(USER,"12312313");
+		Thread.sleep(500);
+		assertTrue(login.checkMsgIsDisplayed(login.errorLoginMsg));
+		
+	}
+}
