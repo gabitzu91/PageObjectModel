a5f02c797c53844bfc479c0a0d20c4e6c55202e6
Gabi <mgluisa9@gmail.com>
2024-05-24 16:07:06 +0300

    curs24

---
+++ src/main/java/Selenium/utils/BaseTest.java
@@ -0,0 +1,30 @@
+package Selenium.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+
+	public WebDriver driver;
+	
+	@BeforeClass
+	public void setup() {
+		//FirefoxDriver()
+		//EdgeDriver()
+		//System.set.property("webdriver.chrome.driver","path to chromedriver.exe")
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://keybooks.ro/");
+		
+	}
+	
+	@AfterClass
+	public void tearDown() throws InterruptedException{
+		Thread.sleep(5000);//bad practice
+		//driver.close();//inchide tab-ul current
+		driver.quit();//inchide fereastra de browser
+	}
+	
+}
