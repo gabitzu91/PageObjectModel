50de9548b609f9d7f924a93b0f760e03eb49d7ad
Gabi <mgluisa9@gmail.com>
2024-06-04 18:01:32 +0300

    curs26 xpath

---
+++ src/main/java/tests/XpathExample.java
@@ -0,0 +1,84 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import Framework.utils.PropertiesFileProcessor;
+import Selenium.utils.BaseTest;
+
+public class XpathExample extends BaseTest{
+
+	String USER = PropertiesFileProcessor.readPropertiesFile("user", "credentials.Properties");
+	String PASS = PropertiesFileProcessor.readPropertiesFile("pass", "credentials.Properties");
+		
+	@Test(priority=1)
+	public void xpathExample1() throws InterruptedException {
+		/*
+		 * CSS:
+		 *  li[class='menu_user_login']
+		 * 
+		 * XPATH:
+		 *   //li[@class='menu_user_login']
+		 *   
+		 */
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		
+		WebElement loginBtn = driver.findElement(
+				By.xpath("//li[@class='menu_user_login']"));
+		
+		jse.executeScript("arguments[0].setAttribute('style', 'background:yellow ;border:10px solid green')", 
+				loginBtn);
+		
+		loginBtn.click();
+		
+		//logical OR
+		WebElement userInput = driver.findElement(
+				By.xpath("//ul//input[@id='log' or @name='log']"));
+		jse.executeScript("arguments[0].setAttribute('style', 'background:yellow ;border:10px solid green')", 
+				userInput);
+		userInput.sendKeys(USER);
+		
+		//logical AND
+		WebElement passInput = driver.findElement(
+				By.xpath("//ul//input[@type='password' and @name='pwd']"));
+		jse.executeScript("arguments[0].setAttribute('style', 'background:yellow ;border:10px solid green')", 
+				passInput);
+		passInput.sendKeys(PASS);
+		
+		//direct child
+		WebElement submitBtn = driver.findElement(
+				By.xpath("//ul//div[@class='popup_form_field submit_field']/input"));
+		submitBtn.click();
+		
+		Thread.sleep(5000);
+	}
+	
+	@Test(priority=2)
+	public void xpathExample2() {
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		driver.findElement(By.xpath("//span[@class='user_name']")).click();
+		
+		//<a href="https://keybooks.ro/wp-admin/profile.php" class="icon icon-cog">Settings</a>
+		/*
+		 *  //a[text()='Settings']
+		 *
+		 */
+		
+		// text() --> referinta catre textul dintre tag-urile HTML
+		driver.findElement(By.xpath("//a[text()='Settings']")).click();
+		
+		/*
+		 *  <a href="https://keybooks.ro/account/orders/">recent orders</a>
+		 *  
+		 *  //a[text()='recent orders']
+		 *  
+		 *  //a[contains(text(),'recent')]
+		 *  
+		 *  //a[contains(@href,'orders']
+		 *  
+		 */
+		
+	}
+}
