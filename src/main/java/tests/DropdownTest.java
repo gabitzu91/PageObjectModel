010c894f3a0436a29e077d3cdb3152b378680e21
Gabi <mgluisa9@gmail.com>
2024-06-04 17:01:19 +0300

    curs26 Select class

---
+++ src/main/java/tests/DropdownTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+import Selenium.utils.BaseTest;
+
+public class DropdownTest extends BaseTest{
+
+	@Test(priority=1)
+	public void selectByValueTest() {
+		app.click(app.menu.shopLink);
+		app.shop.selectByValue("date");
+		assertEquals(app.shop.getSelectedOption(),"Sort by latest");
+	}
+	
+	@Test(priority=2)
+	public void selectByVisibleTextTest() {
+		app.click(app.menu.shopLink);
+		app.shop.selectByVisibleText("Sort by popularity");
+		assertEquals(app.shop.getSelectedOption(),"Sort by popularity");
+	}
+	
+	@Test(priority=3)
+	public void selectByIndexTest() {
+		app.click(app.menu.shopLink);
+		app.shop.selectByIndex(5);
+		assertEquals(app.shop.getSelectedOption(),"Sort by price: high to low");
+	}
+}
